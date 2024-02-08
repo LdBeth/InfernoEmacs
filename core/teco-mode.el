d404ddd32324f2207c742cf534afd8445a93cb2c
LdBeth <andpuke@foxmail.com>
2024-02-08 00:25:00 -0600

    teco-mode: not a dream

--- core/teco-mode.el
+++ core/teco-mode.el
@@ -5,7 +5,6 @@
 (defconst teco-mode-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?! "! 12b" table)
-    (modify-syntax-entry ?/ "|" table)
 
     (modify-syntax-entry ?\" "." table)
     (modify-syntax-entry ?\' "." table)
@@ -14,6 +13,7 @@
     (modify-syntax-entry ?\{ "." table) ; not teco command
     (modify-syntax-entry ?\} "." table)
     (modify-syntax-entry ?*  "." table)
+    (modify-syntax-entry ?/  "." table)
     (modify-syntax-entry ?&  "."  table)
     (modify-syntax-entry ?#  "."  table)
     (modify-syntax-entry ?+  "."  table)
@@ -30,13 +30,29 @@
     table))
 
 (defvar teco-font-lock-keywords
-  '(("[#&*+:@-]" (0 'font-lock-operator-face))
+  '(("[=<>]=\\|<>\\|//\\|<<\\|>>\\|\\^_\\|\\\\/" (0 'font-lock-operator-face))
+    ("[#&*+/!~:@-]" (0 'font-lock-operator-face))
     ("F?['<>|]" (0 font-lock-keyword-face))
     ("\\^[][_\\@A-Za-z]" (0 font-lock-constant-face))
+    ("\\(?:^U\\|[]UXQGM*%\C-u[]\\)\\(\\.?[a-zA-Z0-9]\\)"
+     (1 font-lock-function-name-face))
     ("\"[ACDEFGLNRSTUVW<>=]" (0 font-lock-keyword-face))
     ("F[BCDKNRS_]" (0 font-lock-builtin-face))
-    ("E[ABCFGIJKLMNPQRWXYZ_]" (0 font-lock-builtin-face))
-    ("E[DEHOSTUV]" (0 'font-lock-variable-name-face))))
+    ("E[ABCFGIJKLMNPQRWXYZ%_]" (0 font-lock-builtin-face))
+    ("E[1-4DEHOSTUV]" (0 'font-lock-variable-name-face))
+    ("F?[HZ]\\|[B.]\\|F0" (0 'font-lock-variable-name-face))))
+
+(eval-when-compile
+  (defconst teco-atsign-1-arg-regexp
+    "@[:,0-9]*\\(?:E[%BGILNQRW_]\\|F[BDKR]\\|\\^\\(?:A\\|U\\.?[a-zA-Z0-9]\\)\\|[\1\25=INOS_]\\)\\([^\C-@]\\)[^\C-@]*?\\(\\1\\)")
+  (defconst teco-atsign-2-arg-regexp
+    "@[:,0-9]**\\F[CNS_]\\([^\C-@]\\)[^\C-@]*?\\1[^\C-@]*?\\(\\1\\)"))
+
+(defalias 'teco-mode-syntax-propertize
+  (syntax-propertize-rules
+   (teco-atsign-1-arg-regexp (1 "|") (2 "|"))
+   (teco-atsign-2-arg-regexp (1 "|") (2 "|"))
+   ("@[:,0-9]*\\(!\\)\\([^\C-@]\\)[^\C-@]*?\\(\\1\\)" (1 ".") (2 "!") (3 "!"))))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.te[sc]\\'"  . teco-mode))
@@ -46,4 +62,5 @@
   :syntax-table teco-mode-syntax-table
   (setq font-lock-defaults (list 'teco-font-lock-keywords))
   (setq-local comment-start "! "
-              comment-end " !"))
+              comment-end " !"
+              syntax-propertize-function #'teco-mode-syntax-propertize))
